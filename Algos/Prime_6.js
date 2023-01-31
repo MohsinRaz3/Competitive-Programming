192cea90c6c879f66d931873ef930eca4af8de50
mohsinraz3 <mohsinraz3@gmail.com>
2023-01-31 23:48:32 +0500

    Prime number

---
+++ Algos/Prime_6.js
@@ -0,0 +1,14 @@
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isPrime(5)); //true
+console.log(isPrime(3)); //true
+console.log(isPrime(12)); //false
